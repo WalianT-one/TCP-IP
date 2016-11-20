0b5dc5b5e0f9d14d3c8d1d1905ed5307f3e4394c
Goshtbusters <Goshtbusters@hotmail.fr>
2016-11-20 14:54:24 +0100

    Update FenetreTrame.java

--- FenetreTrame.java
+++ FenetreTrame.java
@@ -5,7 +5,6 @@ public class FenetreTrame extends JFrame{
 	public FenetreTrame(){
 		this.setTitle("Visualisation de la trame");
 		this.setSize(1100, 580);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
